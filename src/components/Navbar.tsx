42872e4f67bf51ffa468757fc63875452a1c2f6f
anirudh <anirudh04mishra@gmail.com>
2022-10-04 19:27:08 +0530

    feat: framer hamburger demo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import '../main.css';
 import '/node_modules/primeflex/primeflex.css'
 import { Button } from './Button';
+import { motion } from "framer-motion"
 
 
 interface NavbarProps {
@@ -12,11 +13,18 @@ interface NavbarProps {
     style?: object,
 }
 
+const variants = {
+    open: { opacity: 1, y: 0 },
+    closed: { opacity: 0, y: "-120%" },
+}
+
+
 export const Navbar = ({
     navItems, start, end, className, style,
     ...props
 }: NavbarProps) => {
     const [mobileNav, showMobileNav] = useState(false)
+    const [animMobileNav, showAnimMobileNav] = useState(false)
     return (
         <div>
             <div style={{ ...style }} className={`flex flex-row bg-white justify-content-between align-items-center ${className} px-3 py-2`} {...props}>
@@ -33,7 +41,12 @@ export const Navbar = ({
                         <Button size='small' variant='filled' mode='primary'>{end}</Button>
                     )}
                 </nav>
-                <Button onClick={() => showMobileNav(!mobileNav)} className='p-0 md:hidden block' variant='text'>
+                <Button onClick={() => {
+                    showAnimMobileNav(!animMobileNav)
+                    if (mobileNav)
+                        setTimeout(() => showMobileNav(!mobileNav), 300)
+                    else showMobileNav(!mobileNav)
+                }} className='p-0 md:hidden block' variant='text'>
                     {mobileNav ? (
                         <img src={require('../assets/cross.svg').default} alt='hamburger'></img>
                     ) : (
@@ -41,7 +54,10 @@ export const Navbar = ({
                     )}
                 </Button>
             </div>
-            <div className={`py-4 px-3 flex-column flex-grow-1 bg-white align-items-start gap-3 ${mobileNav ? 'flex' : 'hidden'}`}>
+            <motion.div
+                animate={animMobileNav ? "open" : "closed"}
+                variants={variants}
+                className={`py-4 px-3 flex-column flex-grow-1 bg-white align-items-start gap-3 ${mobileNav ? 'flex' : 'hidden'}`}>
                 {navItems?.map((item, index) => {
                     return (
                         <Button key={index} className='p-2 text-lg' size='small' variant='text'>{item}</Button>
@@ -50,7 +66,7 @@ export const Navbar = ({
                 {(React.isValidElement(end)) ? end : (
                     <Button className='mt-4 align-self-center' size='medium' variant='filled' mode='primary'>{end}</Button>
                 )}
-            </div>
+            </motion.div>
         </div>
     );
 };
