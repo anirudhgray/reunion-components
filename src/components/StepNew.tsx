ec4503366c7c18a935dcf17af78de87be7a099f1
anirudh <anirudh04mishra@gmail.com>
2022-10-29 18:59:51 +0530

    feat: new steps and icons

---
+++ src/components/StepNew.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import '/node_modules/primeflex/primeflex.css'
+import './stepNew.css';
+
+
+type StepNewProps = {
+    /**
+     * All the steps (strings)
+     */
+    steps?: string[];
+
+    /**
+     * Which step is the user on?
+     */
+    currentStep?: number;
+
+    /**
+     * Color of the progress bar
+     */
+    progressColor?: string;
+
+    /**
+     * Default empty click handler.
+     */
+    onClick?: () => void;
+
+    /**
+     * User styles. Applied on outer div.
+     */
+    style?: object;
+
+    /**
+     * User classnames. Applied on outer div.
+     */
+    className?: string;
+}
+
+export const StepNew: React.FC<StepNewProps> = ({
+    className, style, steps, currentStep = 2, progressColor
+}: StepNewProps) => {
+
+    return (
+        <div style={{ ...style }} className={`flex flex-row gap-1 ${className}`}>
+            {steps?.map((step, index) => {
+                return (
+                    <div className='step-new--step relative'>
+                        <p className='step-new--text'>{index + 1}/{steps.length}: {step}</p>
+                        <hr className='step-new--hr block z-0'></hr>
+                        <hr style={{ 'borderColor': progressColor }} className={`step-new--hr-done absolute bottom-0 z-1 ${currentStep - 1 > index ? 'w-12' : currentStep - 1 < index ? 'w-0' : 'w-6'}`}></hr>
+                    </div>
+                )
+            })}
+        </div>
+    );
+};
---
+++ src/stories/StepNew.stories.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { StepNew } from '../components/StepNew';
+
+export default {
+    title: 'Example/StepNew',
+    component: StepNew,
+    argTypes: {
+    },
+};
+
+const Template = (args) => <StepNew {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    steps: ["Specify Preferences", "Verify Credentials", "Upload Excel"],
+    currentStep: 2,
+};
