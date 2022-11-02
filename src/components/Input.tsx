21c29f02f24838159a190ea504c374053c40640b
anirudh <anirudh04mishra@gmail.com>
2022-11-02 22:22:31 +0530

    fix: navbar style

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -22,7 +22,7 @@ export const Input: React.FC<InputProps> = ({
     return (
         <label style={{ ...labelStyle }} className={`${labelClassName} textinput--label ${error ? 'text--error' : null}`}>
             {label}
-            <input style={{ ...style }} placeholder={placeholder} className={`mt-2 ${endButton ? 'pr-4' : null} block textinput ${className} ${error ? 'textinput--error' : null}`} disabled={disabled}></input>
+            <input style={{ ...style }} placeholder={placeholder} className={`${label ? 'mt-2' : null} ${endButton ? 'pr-4' : null} block textinput ${className} ${error ? 'textinput--error' : null}`} disabled={disabled}></input>
             {endButton ? (
                 <span className='textinput--end-button'>
                     <img alt='endbutton' src={require('../assets/tick.svg').default}></img>
