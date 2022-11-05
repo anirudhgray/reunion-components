a21ce969876910d90000fd4970aba3300b84d918
anirudh <anirudh04mishra@gmail.com>
2022-11-05 19:58:51 +0530

    fix: props

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -17,12 +17,12 @@ type InputProps = {
 }
 
 export const Input: React.FC<InputProps> = ({
-    className, style, label, placeholder, disabled, labelClassName, labelStyle, error, endButton
+    className, style, label, placeholder, disabled, labelClassName, labelStyle, error, endButton, ...props
 }: InputProps) => {
     return (
         <label style={{ ...labelStyle }} className={`${labelClassName} textinput--label ${error ? 'text--error' : null}`}>
             {label}
-            <input style={{ ...style }} placeholder={placeholder} className={`${label ? 'mt-2' : null} ${endButton ? 'pr-4' : null} block textinput ${className} ${error ? 'textinput--error' : null}`} disabled={disabled}></input>
+            <input {...props} style={{ ...style }} placeholder={placeholder} className={`${label ? 'mt-2' : null} ${endButton ? 'pr-4' : null} block textinput ${className} ${error ? 'textinput--error' : null}`} disabled={disabled}></input>
             {endButton ? (
                 <span className='textinput--end-button'>
                     <img alt='endbutton' src={require('../assets/tick.svg').default}></img>
